0812865f4a64bd467c645b6d5d8cc98dd271aed8
Jijin vj <jijinvjvazhippilly@gmail.com>
2023-07-11 09:29:33 +0530

    env setted and check the previous commit

--- src/utilitie/firebaseApiConfiguration.js
+++ src/utilitie/firebaseApiConfiguration.js
@@ -1,17 +1,19 @@
 import { collection, addDoc ,getFirestore,getDocs , deleteDoc ,doc} from "firebase/firestore"; 
+// import dotenv from 'dotenv'
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import {getAuth , GoogleAuthProvider} from "firebase/auth"
-
+console.log(process.env.REACT_APP_APIKEY)
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAT7hOcmbCaA",
-  authDomain: "pass",
-  projectId: "pas8",
-  storageBucket: "paom",
-  messagingSenderId: "19524",
-  appId: "1d1",
-  measurementId: "9ED"
+  apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINDSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREEMENDID
 };
 
 const app = initializeApp(firebaseConfig);
